ef90169b230805190f1bb15d461e0b5a186313d1
Rats12 <senthil1997@gmail.com>
2018-07-08 22:00:08 +0530

    Update league.py

--- league.py
+++ league.py
@@ -1,5 +1,5 @@
 
-api_key='RGAPI-85d3816f-47b6-469a-aeaf-1c934b57d357'
+api_key=''
 region='eune'
 
 import sys
@@ -14,8 +14,11 @@ cs.set_default_region(region)
 summoner =  cs.get_summoner(name= sys.argv[1])
 
 champion=sys.argv[2]
+
 if champion =="Lee":
     champion="Lee Sin"
 if champion =="Xin":
     champion="Xin Zhao"
+    
+   
 print("\n MASTERY POINTS : ",cs.get_champion_mastery(summoner,champion).points)
