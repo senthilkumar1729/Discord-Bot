1af0e3a3496cc3934f6beae3beb8ed4bc0949dcb
Rats12 <senthil1997@gmail.com>
2018-07-08 21:58:57 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,19 +9,19 @@ var sub
 
 
 
-// Configure logger settings
+
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-// Initialize Discord Bot
+
 var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
 bot.editUserInfo( {
-                   username : "Ratbot " });
+                   username : " " });
 
 bot.on('ready', function (evt) {
     
@@ -31,8 +31,7 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
+  
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -79,37 +78,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     message: ' ' + data
                     
-                });})}
+                });})}   
+            
+            args = args.splice(1);
         
-    
-
-            
-            
-            
-        
-        
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'aravind':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'suckmydickaravind!'
-                });
-            break;
-            case 'senthil' :
-                 bot.sendMessage({
-                    to: channelID,
-                    message: 'senthil o great one'});
-                 break;
-                 case 'sada' :
-                 bot.sendMessage({
-                    to: channelID,
-                    message: 'sada 10/10 babe'});
-                 
-            
-         }
      }
 });
 
@@ -120,22 +92,4 @@ const client = new Dis.Client();
 
 client.login(auth.token);
 
-client.on('message', message => {
-  // Voice only works in guilds, if the message does not come from a guild,
-  // we ignore it
-  if (!message.guild) return;
 
-  if (message.content === '!ayy') {
-    // Only try to join the sender's voice channel if they are in one themselves
-    if (message.member.voiceChannel) {
-      message.member.voiceChannel.join()
-        .then(connection => { // Connection is an instance of VoiceConnection
-          message.reply('I have successfully connected to the channel!');
-          const dispatcher = connection.playFile('G:/Music/zombie.mp3');
-        })
-        .catch(console.log);
-    } else {
-      message.reply('You need to join a voice channel first!');
-    }
-  }
-});
