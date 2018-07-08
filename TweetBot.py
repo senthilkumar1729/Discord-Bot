34b906330384a143cf7cf12c173594c0cbd4bbcc
Rats12 <senthil1997@gmail.com>
2018-07-08 21:55:32 +0530

    Update TweetBot.py

--- TweetBot.py
+++ TweetBot.py
@@ -2,12 +2,12 @@ import tweepy
 import sys
 from textblob import TextBlob 
 
-consumer_key = '71VOI1gIAl2GsQjUWExMmf6qn'
-consumer_secret = 'XZE4ONbBYiWUKW8xki7NyATXhQVUVCrOtZVZiOrsjdElEaU7UT'
+consumer_key = ''
+consumer_secret = ''
 
 labels = []
-access_token = '1014110060341952513-nfwnM8Z7I7fRHno7yn8JrM817bFQHi'
-access_token_secret = 'YMI6XMIiiKjwqYNVIYwsnmtaxmsKzuY4rN3OhpnBOhROK'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -15,29 +15,24 @@ auth.set_access_token(access_token, access_token_secret)
 api=tweepy.API(auth)
 
 TweetName = sys.argv[1]
-#TweetName = 'pewdiepie'
+
 
 public_tweets = api.search(TweetName)
 ptweets =[]
 for tweet in public_tweets:
     #print(tweet.text)
-    anal = TextBlob(tweet.text)
-    ptweets.append(anal)
-    if '/' in anal:
-        anal=anal[:anal.find('/')]
-    print(anal)
-    print('*------------------------------------------------------**********************************')
+    tw = TextBlob(tweet.text)
+    ptweets.append(tw)
+    if '/' in tw:
+        tw=tw[:tw.find('/')]
+    print(tw)
+    print('*------------------------------------------------------**')
     print('')
     
-    
-    if anal.sentiment.polarity > 0.3:
-        t = 'positive'
-    elif anal.sentiment.polarity  < 0.4:
-        t = 'negative'
+      
     
   
-    labels.append([tweet.text,t])
-
+    
 
 print(TweetName)
 sys.stdout.flush()
